ad8db1075b88ea5b7f43c4f77ff30ee37f623cf4
alexan1109 <alexan4892@gmail.com>
2024-09-23 22:08:09 +0200

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,49 @@ import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Student from './components/Student'
 import { ofetch } from "ofetch";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+type ProjectProps = {
+  id: number;
+  title: string;
+  company: string;
+  description: String;
+  url: string;
+}
 
-function App({setProjects}: any) {  
+function App() {  
+  const baseProjects: ProjectProps[] = [
+    {
+    id: 0,
+    title: "Project using Javascript",
+    company: "Seriz AS",
+    description: "A website made from scratch with Javascript.",
+    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+
+  {
+    id: 1,
+    title: "Project using Javascript and React",
+    company: "Belum AS",
+    description: "A dynamic website with React.",
+    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    id: 2,
+    title: "Project using Javascript, React and Node.js",
+    company: "Pawn AS",
+    description: "A website with Javascript and Node.js in a React app.",
+    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+
+  {
+    id: 3,
+    title: "Project using Next.js",
+    company: "Labs AS",
+    description: "A website made with Next.js.",
+    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  }];
+  const [projects, setProjects] = useState<ProjectProps[]>(baseProjects);
 
   const initializeData = () => {
     console.log("fetching data");
@@ -27,7 +66,7 @@ function App({setProjects}: any) {
     <div>
     <Student />
     <Experiences />
-    <Projects />
+    <Projects projects={[projects]} setProjects={setProjects} />
     <Contact />
   </div>
   )
--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,50 +1,14 @@
-import {useState, useEffect } from 'react';
 import CreatingProjects from './createProject';
 
-function Projects () {
-  const baseProjects: ProjectProps[] = [
-    {
-    id: 0,
-    title: "Project using Javascript",
-    company: "Seriz AS",
-    description: "A website made from scratch with Javascript.",
-    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  },
-
-  {
-    id: 1,
-    title: "Project using Javascript and React",
-    company: "Belum AS",
-    description: "A dynamic website with React.",
-    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  },
-  {
-    id: 2,
-    title: "Project using Javascript, React and Node.js",
-    company: "Pawn AS",
-    description: "A website with Javascript and Node.js in a React app.",
-    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  },
-
-  {
-    id: 3,
-    title: "Project using Next.js",
-    company: "Labs AS",
-    description: "A website made with Next.js.",
-    url: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  }];
-
-  const [projects, setProjects] = useState<ProjectProps[]>(baseProjects);
+type ProjectProps = {
+  id: number;
+  title: string;
+  company: string;
+  description: String;
+  url: string;
+}
+function Projects ({projects, setProjects}: {projects: any, setProjects: any}) {
     
-  
-  //projects
-  type ProjectProps = {
-    id: number;
-    title: string;
-    company: string;
-    description: String;
-    url: string;
-  }
 
 const ProjectComp  = ({projects}: {projects: Readonly<ProjectProps>}) =>  {
     return(
